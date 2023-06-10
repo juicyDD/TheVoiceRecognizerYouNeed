dbfd03866ae2415e2e295549be13944afb55de1e
juicyDD <ongnguyenuyennhi@gmail.com>
2023-06-10 23:48:19 +0700

    fix edit user page

--- base/api/views.py
+++ base/api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import random, os
